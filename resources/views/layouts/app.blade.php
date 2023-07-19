aa6ad538a45dd645b23f2550f01ab32578549840
yaqub_ajgori <contact.yaqub@gmail.com>
2023-07-19 21:39:40 +0600

    some issue fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,10 @@
      <title>{{ $metaTitle ?: 'Newsbdjob.com | Largest Job Site in Bangladesh' }}</title>
     <meta name="author" content="Newsbdjob.com">
     <meta name="description" content="{{ $metaDescription }}">
-    <link href="{{ asset('favicon.png') }}" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+    <link rel="manifest" href="/site.webmanifest">
     <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap" rel="stylesheet">
 
     <!-- Tailwind -->
